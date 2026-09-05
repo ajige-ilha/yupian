59e7971051817ae79bc408c34746a35e45c30ca6
ajige-ilha <lifeaint1987@hotmail.com>
2026-09-05 23:18:37 +0800

    Passes Prob 2.

--- tests/lisp99/lisp99-list-test.rkt
+++ tests/lisp99/lisp99-list-test.rkt
@@ -1,41 +1,58 @@
 #lang racket/base
 
-(require rackunit
-         racket/runtime-path
-         "../../src/compiler.rkt"
-         "../../src/stdlib.rkt")
+(require 
+    rackunit
+    racket/runtime-path
+    "../../src/compiler.rkt"
+    "../../src/stdlib.rkt"
+    "../../src/lisp99/lisp99_ans.yupian")
 
 (define-runtime-path core-path "../../main.rkt")
 (define-runtime-path stdlib-path "../../src/stdlib.rkt")
+(define-runtime-path path-99 "../../src/lisp99/lisp99_ans.yupian")
 
 (define yupian-namespace (make-base-namespace))
 (parameterize ([current-namespace yupian-namespace])
     (namespace-require `(file ,(path->string core-path)))
-    (namespace-require `(file ,(path->string stdlib-path))))
+    (namespace-require `(file ,(path->string stdlib-path)))
+    (namespace-require `(file ,(path->string path-99)))
+    )
 
 ;;; Problem 1
 (check-equal?
- (eval (s-tree->datum (car (parse-program "「朷 「木 一 二 四 三」」")))
-     yupian-namespace)
- 3)
+    (eval (s-tree->datum (car (parse-program "「朷 「木 一 二 四 三」」")))
+        yupian-namespace)
+    3)
 
 (check-equal?
- (eval (s-tree->datum (car (parse-program "「朷 「木 一」」")))
-     yupian-namespace)
- 1)
+    (eval (s-tree->datum (car (parse-program "「朷 「木 一」」")))
+        yupian-namespace)
+    1)
 
 (check-equal?
- (eval (s-tree->datum (car (parse-program "「朷 卜」")))
-     yupian-namespace)
- '())
+    (eval (s-tree->datum (car (parse-program "「朷 卜」")))
+        yupian-namespace)
+    '())
 
 
 ;;; Problem 2
-(eval (s-tree->datum (car (parse-program "
-    「山 「答二 列」
-        「中
-            『「卜耶 列」 卜』
-            『「卜耶 「本 列」」 卜』
-            『「卜耶 「本 「本 列」」」 「末 列」』
-            『土 「答二 「本 列」」』 
-"))) yupian-namespace)
+(check-equal?
+    (eval (s-tree->datum (car (parse-program "「答二 「木 一 二 四 三」」")))
+        yupian-namespace)
+    4)
+
+(check-equal?
+    (eval (s-tree->datum (car (parse-program "「答二 「木 三 二」」")))
+        yupian-namespace)
+    3)
+
+(check-equal?
+    (eval (s-tree->datum (car (parse-program "「答二 「木 一」」")))
+        yupian-namespace)
+    '())
+
+(check-equal?
+    (eval (s-tree->datum (car (parse-program "「答二 卜」")))
+        yupian-namespace)
+    '())
+
